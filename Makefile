c0520e24cb95660b44d3fd1570280b4b8daf2c84
rwright <rwright>
2000-10-25 18:13:50 +0000

    Initial revision

---
+++ Makefile
@@ -0,0 +1,9 @@
+#Makefile at top of application tree
+TOP = .
+include $(TOP)/config/CONFIG_APP
+DIRS += config
+DIRS += $(wildcard *App)
+DIRS += $(wildcard *app)
+DIRS += $(wildcard iocBoot)
+DIRS += $(wildcard iocboot)
+include $(TOP)/config/RULES_TOP
---
+++ iocBoot/iocether_ip/st.cmd
@@ -0,0 +1,21 @@
+# Example vxWorks startup file
+
+# Following must be added for many board support packages
+#cd <full path to target bin directory>
+
+< cdCommands
+
+#< ../nfsCommands
+
+cd appbin
+ld < iocCore
+ld < seq
+ld < exampleLib
+
+cd startup
+dbLoadDatabase("../../dbd/exampleApp.dbd")
+dbLoadRecords("../../db/dbExample1.db","user=wright")
+dbLoadRecords("../../db/dbExample2.db")
+
+iocInit
+seq &snctest
