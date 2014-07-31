f40ba0d6df35b6d67abd3121bf295464aa6b2867
Kay-Uwe Kasemir <kasemirk@ornl.gov>
2014-07-31 16:26:37 -0400

    Package didn't exclude .hg

--- package.sh
+++ package.sh
@@ -4,7 +4,7 @@ MAYOR=`fgrep "define ETHERIP_MAYOR" ether_ipApp/src/drvEtherIP.h | sed -e 's/.*O
 MINOR=`fgrep "define ETHERIP_MINOR" ether_ipApp/src/drvEtherIP.h | sed -e 's/.*OR //'`
 VERSION=ether_ip-$MAYOR-$MINOR
 cd ..
-gnutar vzcf $VERSION.tgz --exclude .hg/ ether_ip
+gnutar vzcf $VERSION.tgz --exclude .hg ether_ip
 
 echo
 echo
