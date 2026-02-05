epics-modules/ether_ip

commit 4578ba45ab4f775694e0276d8dbc787154d48baa

Fix issue with mbbi record connected to a single number AB tag

ether_ipApp/src/devEtherIP.c
@@ -206,7 +206,7 @@ static eip_bool get_bits(dbCommon *rec, size_t bits, epicsUInt32 *rval)
     }
     /* Fetch bits from BOOL array.
      * #1 directly (faster for BI case), rest in loop */
-    if (value & mask)
+    if ((value & 0x1) & mask)
         *rval = 1;
     for (i=1/*!*/; i<bits; ++i)
     {

commit 5e7cdf2a28f6ac89e1b07667c07eb88fa5ed48c9

fix bi value parse bug introduced in 4578ba4

ether_ipApp/src/devEtherIP.c
@@ -206,8 +206,14 @@ static eip_bool get_bits(dbCommon *rec, size_t bits, epicsUInt32 *rval)
     }
     /* Fetch bits from BOOL array.
      * #1 directly (faster for BI case), rest in loop */
-    if ((value & 0x1) & mask)
-        *rval = 1;
+    if (bits == 1) {
+        if (value & mask)
+           *rval = 1;
+    } else {
+        /* in case the mask is 0xFF */
+        if ((value & 0x1) & mask)
+           *rval = 1;
+    }
     for (i=1/*!*/; i<bits; ++i)
     {
         mask <<= 1;